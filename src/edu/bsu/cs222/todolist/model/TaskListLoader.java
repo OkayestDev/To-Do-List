f2bb447861ffba0e0854d707cbd88b6f93e87917
Qiwei Liu <qliu4@bsu.edu>
2017-04-04 20:37:21 -0400

    cleaned TaskListLoader
    Signed-off-by: Qiwei Liu <qliu4@bsu.edu>

--- src/edu/bsu/cs222/todolist/model/TaskListLoader.java
+++ src/edu/bsu/cs222/todolist/model/TaskListLoader.java
@@ -6,7 +6,6 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -14,28 +13,40 @@ import java.util.Iterator;
 import java.util.List;
 
 public class TaskListLoader {
-    private Document document;
+    private ObservableList<Task> taskList;
+    private Iterator<Element> iterator;
+    private String taskName;
+    private String description;
+    private LocalDate localDate;
+    private Element taskNode;
 
-    public TaskListLoader(String fileName) throws JDOMException, IOException{
+    public TaskListLoader(String fileName) throws JDOMException, IOException {
         SAXBuilder jdomBuilder = new SAXBuilder();
-        document = jdomBuilder.build(new File(fileName));
+        Document document = jdomBuilder.build(new File(fileName));
+        List<Element> taskNodeList = document.getRootElement().getChildren();
+        taskList = FXCollections.observableArrayList();
+        iterator = taskNodeList.iterator();
     }
 
     public ObservableList<Task> load() {
-                List<Element> taskNodeChildrenList = document.getRootElement().getChildren();
-                Iterator<Element> iterator = taskNodeChildrenList.iterator();
-                ObservableList<Task> taskList = FXCollections.observableArrayList();
-                while(iterator.hasNext()) {
-                    Element element = iterator.next();
-                    String taskName = element.getChildText("name");
-                    String description = element.getChildText("description");
-                    String date = element.getChildText("date");
-                    LocalDate localDate = LocalDate.parse(date);
-                    Task newTask = Task.withTaskName(taskName)
-                            .andDescription(description)
-                            .andDate(localDate);
-            taskList.add(newTask);
+        while (iterator.hasNext()) {
+            taskNode = iterator.next();
+            setTask();
+            addTaskToTaskList();
         }
         return taskList;
+    }
+
+    private void setTask() {
+        taskName = taskNode.getChildText("name");
+        description = taskNode.getChildText("description");
+        localDate = LocalDate.parse(taskNode.getChildText("date"));
+    }
+
+    private void addTaskToTaskList() {
+        Task newTask = Task.withTaskName(taskName)
+                .andDescription(description)
+                .andDate(localDate);
+        taskList.add(newTask);
     }
 }
