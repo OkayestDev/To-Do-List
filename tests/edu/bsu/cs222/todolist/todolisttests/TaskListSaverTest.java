2a9b5df10f4ea14ecbff2a2a904584fb7d65c2e2
Qiwei Liu <qliu4@bsu.edu>
2017-04-14 08:36:56 -0400

    change the argument 04 to 4
    Signed-off-by: Qiwei Liu <qliu4@bsu.edu>

--- tests/edu/bsu/cs222/todolist/todolisttests/TaskListSaverTest.java
+++ tests/edu/bsu/cs222/todolist/todolisttests/TaskListSaverTest.java
@@ -30,17 +30,16 @@ public class TaskListSaverTest {
         addTasksToTaskList();
     }
 
-    @SuppressWarnings("OctalInteger, 04 is the time format of LocalDate")
     private void setTasks() {
-        LocalDate localDate1 = LocalDate.of(2017, 04, 17);
+        LocalDate localDate1 = LocalDate.of(2017, 4, 17);
         task1 = Task.withTaskName("Homework")
                 .andDescription("CS222 Homework")
                 .andDate(localDate1);
-        LocalDate localDate2 = LocalDate.of(2017, 04, 18);
+        LocalDate localDate2 = LocalDate.of(2017, 4, 18);
         task2 = Task.withTaskName("Dishes")
                 .andDescription("Do the dishes you bum")
                 .andDate(localDate2);
-        LocalDate localDate3 = LocalDate.of(2017, 04, 19);
+        LocalDate localDate3 = LocalDate.of(2017, 4, 19);
         task3 = Task.withTaskName("CS222 Group Project")
                 .andDescription("Code this test case")
                 .andDate(localDate3);
